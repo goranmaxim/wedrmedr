d94f6c83dae2fe50749370d9844733ea66a4f667
Goran Maksimovski <goran.maksimovski@keitaro.com>
2018-03-25 13:20:30 +0200

    Make weather status importable

--- weather.py
+++ weather.py
@@ -1,15 +1,18 @@
 import pyowm
 import json
 
-api = pyowm.OWM('0d24b5f810c4a57bb781502ca90d6d1a')
 
-latitude = float('41.9973')
-longtitude = float('21.4280')
+def get_conditions():
+    api = pyowm.OWM('0d24b5f810c4a57bb781502ca90d6d1a')
 
-area = api.weather_around_coords(latitude, longtitude)
-locations = []
-for place in area:
-    locations.append(json.loads(place.to_JSON()))
+    latitude = float('41.9973')
+    longtitude = float('21.4280')
 
-weather_conds = [w['Weather']['status'] for w in locations]
-print weather_conds
+    area = api.weather_around_coords(latitude, longtitude)
+
+    locations = []
+    for place in area:
+        locations.append(json.loads(place.to_JSON()))
+
+    weather_conds = set([w['Weather']['status'] for w in locations])
+    return weather_conds
